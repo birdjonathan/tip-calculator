73b356fb50c1dc112258c47542e3d59c2dc29650
topFocus <topfocus@gmx.com>
2016-01-15 00:13:35 -0800

    Will reset bill amount after 10 minutes

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    
+    //************************************************
+    // Lifecycle 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,34 +36,47 @@ class ViewController: UIViewController {
         numberOfPayersControl.autorepeat = true
         numberOfPayersControl.maximumValue = 99
         numberOfPayersControl.minimumValue = 1
+        
+        // Only start tracking the initial moment if we have not stored a start time
+        if defaults.objectForKey("startMoment") == nil {
+            setStartMoment()
+            print("set first start moment")
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
         navigationItem.title = "Tip Calculator"
         super.viewWillAppear(animated)
-        
-        updateTipCalculator()
+        print("View will appear")
+        setInitialTipAndTax()
+        print("set tax and tip")
+        // The following code will reset the calculator if ten minutes have passed
+        let timeUsingApp = timeElapsed()
+        if timeUsingApp < -600 {
+            print("Reset to 0", timeElapsed(), " seconds have passed")
+            billField.text = "0"
+            setBillAmount()
+            updateTipCalculator()
+            setStartMoment()
+        } else {
+            print("Do not reset not enough time yet", timeUsingApp)
+            updateBillAmount()
+            updateTipCalculator()
+        }
         
     }
-
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         self.view.backgroundColor = UIColor.cyanColor()
         print("view did appear")
-        let defaults = NSUserDefaults.standardUserDefaults()
-        
-        // Set default tip and sales tax when app first loads
-        if defaults.objectForKey("defaultTip") == nil {
-            defaults.setInteger(18, forKey: "defaultTip")
-            defaults.setInteger(9, forKey: "defaultSalesTax")
-            defaults.synchronize()
-        }
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         print("Will disappear")
+        setBillAmount()
+        //setStartMoment()
     }
     
     override func viewDidDisappear(animated: Bool) {
@@ -69,38 +88,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func updateTipCalculator (){
-        
-        // Grabs default values from memory
-        let defaults = NSUserDefaults.standardUserDefaults()
-        let initialTipInt = defaults.integerForKey("defaultTip")
-        print(initialTipInt)
-        let initialTipPercentage = Double(initialTipInt)/100
-        let initialSalesTaxInt = defaults.integerForKey("defaultSalesTax")
-        let initialSalesTaxRate = Double(initialSalesTaxInt)/100
-        // Gets number of people to split the tip among from UI label
-        let numberOfPayers = NSString(string: numberOfPayersLabel.text!).doubleValue
-        let billAmount = NSString(string: billField.text!).doubleValue
-        let tax = billAmount * initialSalesTaxRate
-        let checkTotal = billAmount + tax
-        let tip = checkTotal * initialTipPercentage
-        let finalTotal = checkTotal + tip
-        let splitAmount = tip / numberOfPayers
-        
-        // Sets the correct value for all the labels
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(finalTotal)"
-        splitLabel.text = "$\(splitAmount)"
-        taxLabel.text = "$\(tax)"
-        
-        // Formats the labels to two decimal places
-        tipLabel.text = String(format: "$%.2f", tip)
-        taxLabel.text = String(format: "$%.2f", tax)
-        totalLabel.text = String(format: "$%.2f", finalTotal)
-        splitLabel.text = String(format: "$%.2f", splitAmount)
 
-    }
+    //***************************************************
+    // Actions
     
     @IBAction func onEditingChanged(sender: AnyObject) {
         updateTipCalculator()
@@ -128,11 +118,80 @@ class ViewController: UIViewController {
         
     }
     
-    
-    
     @IBAction func onTap(sender: AnyObject) {
         
         view.endEditing(true)
+    }
+    
+    //***********************************************************
+    //Helper Functions
+    
+    func setStartMoment () {
+        let currentTime = NSDate()
+        defaults.setObject(currentTime, forKey: "startMoment")
+        defaults.synchronize()
+    }
+    
+    func timeElapsed () -> Int{
+        let startMoment = defaults.objectForKey("startMoment")
+        let now = NSDate()
+        let elapsed = startMoment?.timeIntervalSinceDate(now)
+        print("Elapsed time", Int(elapsed!))
+        return Int(elapsed!)
+    }
+    
+    func setBillAmount () {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let billAmount = NSString(string: billField.text!).doubleValue
+        defaults.setDouble(billAmount, forKey: "billAmount")
+        defaults.synchronize()
+    }
+    
+    func updateBillAmount () {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        //return defaults.doubleForKey("billAmount")
+        billField.text = defaults.stringForKey("billAmount")
+    }
+    
+    func setInitialTipAndTax () {
+        if defaults.objectForKey("defaultTip") == nil {
+            defaults.setInteger(18, forKey: "defaultTip")
+            defaults.setInteger(9, forKey: "defaultSalesTax")
+            defaults.synchronize()
+        }
+    }
+    
+    func updateTipCalculator (){
+        
+        // Grabs default values from memory
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let initialTipInt = defaults.integerForKey("defaultTip")
+        print(initialTipInt)
+        let initialTipPercentage = Double(initialTipInt)/100
+        let initialSalesTaxInt = defaults.integerForKey("defaultSalesTax")
+        let initialSalesTaxRate = Double(initialSalesTaxInt)/100
+        // Gets number of people to split the tip among from UI label
+        let numberOfPayers = NSString(string: numberOfPayersLabel.text!).doubleValue
+        //let billAmount = defaults.doubleForKey("billAmount")
+        let billAmount =  NSString(string: billField.text!).doubleValue
+        let tax = billAmount * initialSalesTaxRate
+        let checkTotal = billAmount + tax
+        let tip = checkTotal * initialTipPercentage
+        let finalTotal = checkTotal + tip
+        let splitAmount = tip / numberOfPayers
+        
+        // Sets the correct value for all the labels
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(finalTotal)"
+        splitLabel.text = "$\(splitAmount)"
+        taxLabel.text = "$\(tax)"
+        
+        // Formats the labels to two decimal places
+        tipLabel.text = String(format: "$%.2f", tip)
+        taxLabel.text = String(format: "$%.2f", tax)
+        totalLabel.text = String(format: "$%.2f", finalTotal)
+        splitLabel.text = String(format: "$%.2f", splitAmount)
+        
     }
 
 }
